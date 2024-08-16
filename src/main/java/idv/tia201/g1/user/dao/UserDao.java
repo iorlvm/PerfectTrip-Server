a6c19c9715b526e3fd0f684901b1ae0ec26b3ef1
Chih Chien Hsieh <twcch1218@gmail.com>
2024-08-16 22:11:30 +0800

    新增 UserDao 註解

--- src/main/java/idv/tia201/g1/user/dao/UserDao.java
+++ src/main/java/idv/tia201/g1/user/dao/UserDao.java
@@ -1,26 +1,49 @@
 package idv.tia201.g1.user.dao;
 
-import idv.tia201.g1.constant.UserGroup;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import idv.tia201.g1.entity.User;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Integer> {
 
+    /**
+     * Finds a user by their user ID.
+     *
+     * @param userId the ID of the user to find
+     * @return the User object with the specified user ID
+     */
     public User findByUserId(Integer userId);
 
+    /**
+     * Finds a user by their username.
+     *
+     * @param username the username of the user to find
+     * @return the User object with the specified username
+     */
     public User findByUsername(String username);
 
+    /**
+     * Finds a user by their tax ID.
+     *
+     * @param taxId the tax ID of the user to find
+     * @return the User object with the specified tax ID
+     */
     public User findByTaxId(String taxId);
 
+    /**
+     * Finds a user by their phone number.
+     *
+     * @param phoneNumber the phone number of the user to find
+     * @return the User object with the specified phone number
+     */
     public User findByPhoneNumber(String phoneNumber);
 
+    /**
+     * Counts the total number of users.
+     *
+     * @return the total number of users
+     */
     public long count();
 
 }
