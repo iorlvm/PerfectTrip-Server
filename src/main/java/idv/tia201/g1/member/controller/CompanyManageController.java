713a0399752472e2fabdf1aa46c5e9517c3f62c4
Felicie <felicieavbg@gmail.com>
2024-09-27 20:26:03 +0800

    完成CompanyManageController

--- src/main/java/idv/tia201/g1/member/controller/CompanyManageController.java
+++ src/main/java/idv/tia201/g1/member/controller/CompanyManageController.java
@@ -3,43 +3,38 @@ package idv.tia201.g1.member.controller;
 
 import idv.tia201.g1.core.dto.Result;
 import idv.tia201.g1.member.dto.CompanyEditDetailRequest;
-import idv.tia201.g1.member.entity.Company;
-import idv.tia201.g1.member.entity.CompanyPhotos;
+import idv.tia201.g1.member.dto.CompanyEditDetailResponse;
 import idv.tia201.g1.member.service.CompanyManagerService;
 import jakarta.validation.Valid;
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
-@RequestMapping("/api/store")
+@RequestMapping("/api/storeEdit")
 public class CompanyManageController {
     @Autowired
     private CompanyManagerService companyManagerService;
 
 
-//    @PostMapping("/{companyId}")
-//    public Result addCompanyEditDetail(@RequestBody @Valid CompanyEditDetailRequest companyEditDetailRequest){
-//        CompanyEditDetailRequest editDetail = companyManagerService.handleEditCompanyInfo(companyEditDetailRequest);
-//    }
+    //更新編輯資訊
+    @PutMapping("/{companyId}")
+    public Result handleEditCompanyInfo(@RequestBody @Valid CompanyEditDetailRequest companyEditDetailRequest) {
+        // 調用 service 來處理公司信息更新
+        companyManagerService.handleEditCompanyInfo(companyEditDetailRequest);
+        return Result.ok("公司信息已成功更新");
+    }
 
-    //新增設施
+    //取得編輯資訊
+    @GetMapping("/{companyId}")
+    public Result getCompanyDetail( @PathVariable Integer companyId,@RequestParam @Valid CompanyEditDetailResponse companyEditDetailResponse){
+        CompanyEditDetailResponse companyDetail = companyManagerService.getCompanyDetail(companyId);
+        return Result.ok(companyDetail);
+    }
 
 
-    //新增文字
 
-
-    //修改照片/修改設施/修改文字
-
-
-    //查 獲得照片列表
-
-    //查  獲得設施列表
-
-    //查 用companyid 獲得商家資料(照片、設施、文字)
 
 
 }
--- src/main/java/idv/tia201/g1/member/service/CompanyManagerService.java
+++ src/main/java/idv/tia201/g1/member/service/CompanyManagerService.java
@@ -1,12 +1,10 @@
 package idv.tia201.g1.member.service;
-
 import idv.tia201.g1.member.dto.CompanyEditDetailRequest;
 import idv.tia201.g1.member.dto.CompanyEditDetailResponse;
-import idv.tia201.g1.member.dto.CompanyUpdateRequest;
 import idv.tia201.g1.member.entity.Company;
 import idv.tia201.g1.member.entity.CompanyFacility;
 import idv.tia201.g1.member.entity.CompanyPhotos;
-import idv.tia201.g1.product.entity.Facility;
+
 
 
 import java.util.List;
@@ -19,12 +17,13 @@ public interface CompanyManagerService {
     //TODO將所有的商店與設施關聯刪掉 DELET FACILITY BY COMPANY_ID  新增FACILITTY LIST
     void handleEditCompanyInfo(CompanyEditDetailRequest companyEditDetailRequest);
 
-    //取得照片
+    //取得照片,查詢(大家都可查)
     List<CompanyPhotos> getCompanyPhotos(Company companyId);
 
     //取得設施
     List<CompanyFacility> getCompanyFacilities(Integer companyId);
 
     CompanyEditDetailResponse getCompanyDetail (Integer companyId);
+
 
 }
